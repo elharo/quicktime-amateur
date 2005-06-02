4c679b1b80c476e2d961d2b5238595b8eae41007
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2005-06-02 15:44:50 +0000

    Some clipboard support (Copy and Clear) may not work yet

---
+++ src/com/elharo/quicktime/CopyAction.java
@@ -0,0 +1,41 @@
+/*
+ * Created on Jun 2, 2005
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package com.elharo.quicktime;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.KeyStroke;
+
+import quicktime.QTException;
+import quicktime.std.movies.Movie;
+import quicktime.std.movies.MovieController;
+
+class CopyAction extends AbstractAction {
+
+    private Movie movie;
+
+    CopyAction(Movie movie) {
+        this.movie = movie;
+        putValue(Action.NAME, "Copy");  
+        putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke('C', PlayerFrame.menuShortcutKeyMask));  
+    } 
+    
+    
+    public void actionPerformed(ActionEvent arg0) {
+        try {
+            movie.copySelection();
+            // XXX does this get on the clipboard automatically? or do we need to put it there?
+        }
+        catch (QTException e) {
+            // ???? Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+}
