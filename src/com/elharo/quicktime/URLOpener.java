bbf6e8efce7607b5c3863700494f64bcb93292a6
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2005-06-03 12:22:31 +0000

    Adding GPL notices

--- src/com/elharo/quicktime/URLOpener.java
+++ src/com/elharo/quicktime/URLOpener.java
@@ -1,9 +1,23 @@
-/*
- * Created on May 28, 2005
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
+/* Copyright 2005 Elliotte Rusty Harold
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+You can contact Elliotte Rusty Harold by sending e-mail to
+elharo@metalab.unc.edu. Please include the word "Amateur" in the
+subject line. The Amateur home page is located at http://www.elharo.com/amateur/
+*/
 package com.elharo.quicktime;
 
 import java.awt.event.ActionEvent;
