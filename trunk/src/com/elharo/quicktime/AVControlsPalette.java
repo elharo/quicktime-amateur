0802be23e2b28087e4f7c25733fa0771803ed325
elharo <elharo@6614a4d6-7ad5-e0f1-e44c-94ff3c1482c0>
2005-10-13 14:00:09 +0000

    Improving fonts on labels; adding labels to bottom sliders

--- trunk/src/com/elharo/quicktime/AVControlsPalette.java
+++ trunk/src/com/elharo/quicktime/AVControlsPalette.java
@@ -21,6 +21,8 @@ subject line. The Amateur home page is located at http://www.elharo.com/amateur/
 package com.elharo.quicktime;
 
 import java.awt.*;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 import javax.swing.*;
 import javax.swing.border.Border;
@@ -43,31 +45,43 @@ final class AVControlsPalette extends JDialog {
     private AVControlsPalette() {
         
         this.setTitle("A/V Controls");
+        
+        Font labelFont = new Font("Helvetica", Font.BOLD, 11);
+        Font boxFont = new Font("Helvetica", Font.PLAIN, 12);
+        
+        Color bgcolor = new Color(225, 225, 225);
+        
         this.getContentPane().setLayout(new BorderLayout());
         
         JPanel audioControls = new JPanel();
         Border border = BorderFactory.createLineBorder(Color.GRAY);
         audioControls.setBorder(BorderFactory.createTitledBorder(border, "Audio", TitledBorder.LEFT, TitledBorder.ABOVE_TOP));
-        audioControls.setBackground(Color.LIGHT_GRAY);
+        audioControls.setBackground(bgcolor);
         audioControls.setLayout(new GridLayout(10, 1));
         
         int min = 0;
         int max = 10;
 
-        audioControls.add(new JLabel("Volume"));
+        JLabel volumeLabel = new JLabel("Volume");
+        volumeLabel.setFont(labelFont);
+        audioControls.add(volumeLabel);
         JSlider volume = new JSlider(JSlider.HORIZONTAL, min, max, 5 );
         volume.setPaintTicks( true );
         volume.setMajorTickSpacing( 5 );
         audioControls.add(volume);
         audioControls.add(new JSeparator());
 
-        audioControls.add(new JLabel("Bass"));
+        JLabel bassLabel = new JLabel("Bass");
+        bassLabel.setFont(labelFont);
+        audioControls.add(bassLabel);
         JSlider bass = new JSlider(JSlider.HORIZONTAL, min, max, 5 );
         bass.setPaintTicks( true );
         bass.setMajorTickSpacing( 5 );
         audioControls.add(bass);
         
-        audioControls.add(new JLabel("Treble"));
+        JLabel trebleLabel = new JLabel("Treble");
+        trebleLabel.setFont(labelFont);
+        audioControls.add(trebleLabel);
         JSlider treble = new JSlider(JSlider.HORIZONTAL, min, max, 5 );
         treble.setPaintTicks( true );
         treble.setMajorTickSpacing( 5 );
@@ -75,39 +89,48 @@ final class AVControlsPalette extends JDialog {
         
         audioControls.add(new JSeparator());
 
-        audioControls.add(new JLabel("Balance"));
+        JLabel balanceLabel = new JLabel("Balance");
+        balanceLabel.setFont(labelFont);
+        audioControls.add(balanceLabel);
         JSlider balance = new JSlider(JSlider.HORIZONTAL, min, max, 5 );
         balance.setPaintTicks( true );
         balance.setMajorTickSpacing( 5 );
         audioControls.add(balance);
         
-        westPanel.add(BorderLayout.SOUTH, audioControls);
-  
+        westPanel.add(BorderLayout.SOUTH, audioControls);  
         
         JPanel videoControls = new JPanel();
         videoControls.setBorder(BorderFactory.createTitledBorder(border, "Video", TitledBorder.LEFT, TitledBorder.ABOVE_TOP));
-        videoControls.setBackground(Color.LIGHT_GRAY);
+        videoControls.setBackground(bgcolor);
         videoControls.setLayout(new GridLayout(10, 1));
 
-        videoControls.add(new JLabel("Brightness"));
+        JLabel brightnessLabel = new JLabel("Brightness");
+        brightnessLabel.setFont(labelFont);
+        videoControls.add(brightnessLabel);
         JSlider brightness = new JSlider(JSlider.HORIZONTAL, min, max, 5 );
         brightness.setPaintTicks( true );
         brightness.setMajorTickSpacing( 5 );
         videoControls.add(brightness);
 
-        videoControls.add(new JLabel("Color"));
+        JLabel colorLabel = new JLabel("Color");
+        colorLabel.setFont(labelFont);
+        videoControls.add(colorLabel);
         JSlider color = new JSlider(JSlider.HORIZONTAL, min, max, 5 );
         color.setPaintTicks( true );
         color.setMajorTickSpacing( 5 );
         videoControls.add(color);
 
-        videoControls.add(new JLabel("Contrast"));
+        JLabel contrastLabel = new JLabel("Contrast");
+        contrastLabel.setFont(labelFont);
+        videoControls.add(contrastLabel);
         JSlider contrast = new JSlider(JSlider.HORIZONTAL, min, max, 5 );
         contrast.setPaintTicks( true );
         contrast.setMajorTickSpacing( 5 );
         videoControls.add(contrast);
         
-        videoControls.add(new JLabel("Tint"));
+        JLabel tintLabel = new JLabel("Tint");
+        tintLabel.setFont(labelFont);
+        videoControls.add(tintLabel);
         JSlider tint = new JSlider(JSlider.HORIZONTAL, min, max, 5 );
         tint.setPaintTicks( true );
         tint.setMajorTickSpacing( 5 );
@@ -115,36 +138,67 @@ final class AVControlsPalette extends JDialog {
 
         videoControls.add(new JButton("Reset"));
         
-        eastPanel.add(BorderLayout.SOUTH, videoControls);
-         
+        eastPanel.add(BorderLayout.SOUTH, videoControls);         
         
         JPanel playbackControls = new JPanel();
         playbackControls.setBorder(BorderFactory.createTitledBorder(border, 
           "Playback", TitledBorder.LEFT, TitledBorder.ABOVE_TOP));
-        playbackControls.setBackground(Color.LIGHT_GRAY);
+        playbackControls.setBackground(bgcolor);
         
         
+        Font sliderLabelFont = new Font("Helvetica", Font.PLAIN, 11);
+
         JPanel jog = new JPanel();
         jog.setLayout(new GridLayout(2, 1));
-        jog.add(new JLabel("Jog Shuttle"));
+        JLabel jogLabel = new JLabel("Jog Shuttle");
+        jogLabel.setFont(labelFont);
+        jog.add(jogLabel);
         JSlider jogShuttle = new JSlider(JSlider.HORIZONTAL, min, max, 5 );
         jogShuttle.setPaintTicks( true );
         jogShuttle.setMajorTickSpacing( 5 );
         jog.add(jogShuttle);
-        jog.setBackground(Color.LIGHT_GRAY);
+        jog.setBackground(bgcolor);
+        Dictionary jogLabels = new Hashtable();
+        JLabel reverse = new JLabel("Reverse");
+        reverse.setFont(sliderLabelFont);
+        jogLabels.put(new Integer(min), reverse);
+        JLabel forward = new JLabel("Forward");
+        forward.setFont(sliderLabelFont);
+        jogLabels.put(new Integer(max), forward);
+        jogShuttle.setLabelTable(jogLabels);
+        jogShuttle.setPaintLabels(true);
         playbackControls.add(jog);
         
         playbackControls.add(new JSeparator(SwingConstants.VERTICAL));
         
         JPanel speed = new JPanel();
         speed.setLayout(new GridLayout(2, 1));
-        speed.add(new JLabel("Playback Speed"));
-        JSlider playbackSpeed = new JSlider(JSlider.HORIZONTAL, 0, 6, 2 );
+        JLabel speedLabel = new JLabel("Playback Speed");
+        speedLabel.setFont(labelFont);
+        speed.add(speedLabel);
+        JSlider playbackSpeed = new JSlider(JSlider.HORIZONTAL, 1, 6, 2 );
         playbackSpeed.setPaintTicks( true );
         playbackSpeed.setMajorTickSpacing( 2 );
         playbackSpeed.setMinorTickSpacing( 1 );
         speed.add(playbackSpeed);
-        speed.setBackground(Color.LIGHT_GRAY);
+        speed.setBackground(bgcolor);
+
+        Dictionary speedLabels = new Hashtable();
+        JLabel half = new JLabel("1/2x");
+        half.setFont(sliderLabelFont);
+        speedLabels.put(new Integer(1), half);
+        JLabel one = new JLabel("1x");
+        one.setFont(sliderLabelFont);
+        speedLabels.put(new Integer(2), one);
+        JLabel two = new JLabel("2x");
+        two.setFont(sliderLabelFont);
+        speedLabels.put(new Integer(4), two);
+        JLabel three = new JLabel("3x");
+        three.setFont(sliderLabelFont);
+        speedLabels.put(new Integer(6), three);
+        playbackSpeed.setLabelTable(speedLabels);
+        playbackSpeed.setPaintLabels(true);
+        
         playbackControls.add(speed);
         
         // playbackControls.setLayout(new GridLayout(1, 3));
