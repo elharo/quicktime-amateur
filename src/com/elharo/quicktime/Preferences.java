fee46a03df5acbd1bb0a979722bf4f71308df02c
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2006-07-10 14:45:29 +0000

    Adding setter methods

--- src/com/elharo/quicktime/Preferences.java
+++ src/com/elharo/quicktime/Preferences.java
@@ -5,6 +5,16 @@ public class Preferences {
     private static Preferences instance = new Preferences();
     
     // XXX are these sticky over restarts? should they be?
+    // defaults
+    private boolean openMoviesInNewPlayers = true;
+    private int     numberOfRecentItems = 10;
+    private boolean pauseMoviesBeforeSwitchingPlayers = true;
+    private boolean showContentGuideAutomatically = false;
+    private boolean showEqualizer = true;
+    private boolean playSoundWhenApplicationIsInBackground = true;
+    private boolean playSoundInFrontmostPlayerOnly = true;
+    private boolean useHighQualityVideo = true;
+    
     
     private Preferences() {}
     
@@ -13,35 +23,69 @@ public class Preferences {
     }
 
     public boolean getOpenMoviesInNewPlayers() {
-        return true;
+        return openMoviesInNewPlayers;
     }
 
     public boolean getUseHighQualityVideo() {
-        return true;
+        return this.useHighQualityVideo;
     }
 
     public boolean getShowContentGuideAutomatically() {
-        return false;
+        return this.showContentGuideAutomatically ;
     }
 
     public boolean getPlaySoundInFrontmostPlayerOnly() {
-        return true;
+        return this.playSoundInFrontmostPlayerOnly;
     }
 
     public boolean getPlaySoundWhenApplicationIsInBackground() {
-        return true;
+        return this.playSoundWhenApplicationIsInBackground;
     }
 
     public int getNumberOfRecentItems() {
-        return 10;
+        return this.numberOfRecentItems;
     }
 
     public boolean getPauseMoviesBeforeSwitchingPlayers() {
-        return true;
+        return this.pauseMoviesBeforeSwitchingPlayers;
     }
 
     public boolean getShowEqualizer() {
-        return true;
+        return this.showEqualizer;
+    }
+
+    public void setOpenMoviesInNewPlayers(boolean b) {
+        this.openMoviesInNewPlayers = b;
+    }
+
+    public void setNumberOfRecentItems(int i) {
+        this.numberOfRecentItems = i;
+        
+    }
+
+    public void setPauseMoviesBeforeSwitchingPlayers(boolean b) {
+        this.pauseMoviesBeforeSwitchingPlayers  = b;
+        
+    }
+
+    public void setShowContentGuideAutomatically(boolean b) {
+        this.showContentGuideAutomatically = b;
+    }
+
+    public void setShowEqualizer(boolean b) {
+        this.showEqualizer  = b;
+    }
+
+    public void setPlaySoundWhenApplicationIsInBackground(boolean b) {
+        this.playSoundWhenApplicationIsInBackground = b;
+    }
+
+    public void setPlaySoundInFrontmostPlayerOnly(boolean b) {
+        this.playSoundInFrontmostPlayerOnly = b;
+    }
+
+    public void setUseHighQualityVideo(boolean b) {
+        this.useHighQualityVideo = b;
     }
 
 }
