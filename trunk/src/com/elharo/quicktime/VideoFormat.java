20c7bbcdcc982874270581d890c16e49ee637cb0
elharo <elharo@6614a4d6-7ad5-e0f1-e44c-94ff3c1482c0>
2006-07-17 13:05:33 +0000

    Adding other video formats

--- trunk/src/com/elharo/quicktime/VideoFormat.java
+++ trunk/src/com/elharo/quicktime/VideoFormat.java
@@ -6,8 +6,7 @@ import quicktime.util.QTUtils;
 
 
 /**
- * See https://developer.apple.com/documentation/QuickTime/QTFF/QTFFChap3/chapter_4_section_3.html
- * especially the table at the bottom
+ * See https://developer.apple.com/documentation/QuickTime/QTFF/QTFFChap3/chapter_4_section_2.html#//apple_ref/doc/uid/TP40000939-CH205-74522
  */
 public class VideoFormat {
 
@@ -18,7 +17,19 @@ public class VideoFormat {
     static {
         codes.put(new Integer(0x53565133), new VideoFormat(0x53565133, "", "Sorenson Video 3 Decompressor"));
         codes.put(new Integer(0x6d703476), new VideoFormat(0x6d703476, "Apple MPEG4 Decompressor", "Apple MPEG 4 Decompressor"));
-    }
+        codes.put(new Integer(QTUtils.toOSType("cvid")), new VideoFormat("cvid", "Cinepak", "Cinepak"));
+        codes.put(new Integer(QTUtils.toOSType("jpeg")), new VideoFormat("jpeg", "JPEG", "JPEG"));
+        codes.put(new Integer(QTUtils.toOSType("raw ")), new VideoFormat("raw ", "Uncompressed RGB", "Uncompressed RGB"));
+        codes.put(new Integer(QTUtils.toOSType("Yuv2")), new VideoFormat("Yuv2", "Uncompressed YUV422", "Uncompressed YUV422"));
+        codes.put(new Integer(QTUtils.toOSType("smc ")), new VideoFormat("smc ", "Graphics", "Graphics"));
+        codes.put(new Integer(QTUtils.toOSType("rle ")), new VideoFormat("rle ", "Animation", "Animation"));
+        codes.put(new Integer(QTUtils.toOSType("rpza")), new VideoFormat("rpza", "Apple video", "Apple video"));
+        codes.put(new Integer(QTUtils.toOSType("kpcd")), new VideoFormat("kpcd", "Kodak Photo CD", "Kodak Photo CD"));
+        codes.put(new Integer(QTUtils.toOSType("mpeg")), new VideoFormat("mpeg", "MPEG", "MPEG"));
+        codes.put(new Integer(QTUtils.toOSType("mjpa")), new VideoFormat("mjpa", "Motion-JPEG (new VideoFormat(format A)", "Motion-JPEG (new VideoFormat(format A)"));
+        codes.put(new Integer(QTUtils.toOSType("mjpb")), new VideoFormat("mjpb", "Motion-JPEG (new VideoFormat(format B)", "Motion-JPEG (new VideoFormat(format B)"));
+        codes.put(new Integer(QTUtils.toOSType("svqi")), new VideoFormat("svqi", "Sorenson video", "Sorenson video"));
+   }
     
     private int code;
     private String description;
